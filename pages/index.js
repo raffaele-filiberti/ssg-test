c33c35b5c5f1fb9146ced8dbbe477ab8125db5f1
Raffaele Filiberti <raffaele.filiberti@aquest.it>
2021-01-11 15:51:18 +0100

    test watchtower

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function IndexPage({ postList }) {
         <title>Legendary Home page</title>
       </Head>
 
-      <h1>Awesome List of posts</h1>
+      <h1>Awesome List of posts updated by watchtower</h1>
 
       <section>
         {postList.map((post) => (
