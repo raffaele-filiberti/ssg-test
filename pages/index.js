e45d9ea22f081a29cdd19b053e7fd09e7852c543
Raffaele Filiberti <raffaele.filiberti@gmail.com>
2021-01-20 09:46:34 +0100

    fix(test): fixing title

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function IndexPage({ postList }) {
         <title>Legendary Home page</title>
       </Head>
 
-      <h1>Versioned dev img</h1>
+      <h1>Versioned img based on branches</h1>
 
       <section>
         {postList.map((post) => (
