79890ddfd396e0296685e186a4e81a73791bbb43
Raffaele Filiberti <raffaele.filiberti@gmail.com>
2021-01-20 17:37:48 +0100

    fix(ci): add github release plugin

--- release.config.js
+++ release.config.js
@@ -6,13 +6,20 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
+      "@semantic-release/github",
+      {
+        "successComment": false,
+        "failComment": false
+      }
+    ],
+    [
       '@semantic-release/git',
       {
         assets: [
           'CHANGELOG.md',
           'package.json',
         ],
-        message: 'chore(release): ${nextRelease.version} [skip ci]',
+        message: 'chore(release): ${branch.name}@${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
